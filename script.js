18cf627f0574d633c1f99c5097528170736af8ce
sumedhahegde41-bit <sumedhahegde41@gmail.com>
2026-07-30 13:47:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
+const navbar = document.querySelector("header");
 
+window.addEventListener("scroll", () => {
+
+if(window.scrollY > 60){
+
+navbar.classList.add("scrolled");
+
+}else{
+
+navbar.classList.remove("scrolled");
+
+}
+
+});
+const topBtn=document.getElementById("topBtn");
+
+window.addEventListener("scroll",()=>{
+
+if(window.scrollY>500){
+
+topBtn.style.display="block";
+
+}else{
+
+topBtn.style.display="none";
+
+}
+
+});
+
+topBtn.onclick=function(){
+
+window.scrollTo({
+
+top:0,
+
+behavior:"smooth"
+
+});
+
+};
